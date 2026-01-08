7a91ce7fd4de152890eda28024e171c95976f937
kartavkun <nikitarobezhko@yandex.ru>
2026-01-08 17:11:43 +0500

    Add info about spotify oauth

--- src/trackcast/auth/spotify_auth.py
+++ src/trackcast/auth/spotify_auth.py
@@ -1,12 +1,12 @@
-import os
 import time
 import keyring as kr
 from spotipy.oauth2 import SpotifyOAuth
+from trackcast.auth.spotify_cred import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 
 SERVICE = "TrackCast"
 
-CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+CLIENT_ID = SPOTIFY_CLIENT_ID
+CLIENT_SECRET = SPOTIFY_CLIENT_SECRET
 REDIRECT_URI = "http://127.0.0.1:8080/callback"
 SCOPE = "user-read-currently-playing"
 
